7bbc3df09a6f44cab63bc6096290667d447fe22f
Pernelle <alain.pernelle@gmail.com>
2015-08-07 19:39:48 +0200

    del nginx/config/nginx.conf must be generate by setu-nginx-config.sh

--- setup-nginx-config.sh
+++ setup-nginx-config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Variable for local host
-HOSTIPNAME=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')^C
+HOSTIPNAME=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
 cat << EOF > nginx/config/nginx.conf
 daemon off;
 
